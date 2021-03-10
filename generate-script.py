e44e555ec745dfdcb4d15de2103089ca1a679552
Ane794 <Ane794@outlook.com>
2021-03-10 15:12:48 +0800

    仅当直接运行该文件时执行操作

--- generate-script.py
+++ generate-script.py
@@ -3,24 +3,23 @@
 import configparser
 
 _BAT_URL: str = 'backup.bat'
-_ROOT_INFO_URL: str = 'conf\\root.cfg'
-_DIRS_MAP_URL: str = 'conf\\dirs.cfg'
+_ROOT_INFO_URL: str = r'conf\root.cfg'
+_DIRS_MAP_URL: str = r'conf\dirs.cfg'
 
 _MODE_STR: str = 'MODE'
 _SRC_ROOT_STR: str = 'SRC_ROOT'
 _DEST_ROOT_STR: str = 'DEST_ROOT'
 
-_UTF_8: str = 'UTF-8'
+_ENCODING = 'UTF-8'
 
 _ROBOCOPY_ARGS: str = '/mir /unicode'
 
-
-def _run():
-    with open(_BAT_URL, 'w', encoding=_UTF_8) as bat_file:
+if __name__ == '__main__':
+    with open(_BAT_URL, 'w', encoding=_ENCODING) as bat_file:
         bat_file.write('@echo off\n')
 
         conf = configparser.ConfigParser()
-        conf.read(_ROOT_INFO_URL, _UTF_8)
+        conf.read(_ROOT_INFO_URL, _ENCODING)
         enabled = conf.get('mode', 'ENABLED') == 'True'
         if enabled:
             src_root = conf.get('roots', _SRC_ROOT_STR)
@@ -29,7 +28,7 @@ def _run():
             bat_file.write('set ' + _DEST_ROOT_STR + '=' + dest_root + '\n')
 
         conf = configparser.ConfigParser()
-        conf.read(_DIRS_MAP_URL, _UTF_8)
+        conf.read(_DIRS_MAP_URL, _ENCODING)
         dirs = conf.items('dirs')
         for item in dirs:
             if enabled:
@@ -44,7 +43,4 @@ def _run():
                       + ' ' + _ROBOCOPY_ARGS
 
             bat_file.write(cmd + '\n')
-
-
-_run()
-exit(0)
+            exit(0)
