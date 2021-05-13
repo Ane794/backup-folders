64e11295c519ad62751158f6608c2d5962ebb859
Ane794 <Ane794@outlook.com>
2021-05-13 22:54:14 +0800

    修改代码结构。

--- start.bat
+++ start.bat
@@ -1,6 +1,4 @@
 @echo off
-echo 根据 "config.yml" 生成备份脚本...
-echo 将备份以下目录:
 
 set GENERATE_SCRIPT=".\generate-script.py"
 set SIMPLIFY_LOG_SCRIPT=".\simplify-log.py"
@@ -8,44 +6,74 @@ set BACKUP_SCRIPT=".\backup.bat"
 set LOG_DIR=".\log"
 set LOG="%LOG_DIR%\backup.log"
 
-python %GENERATE_SCRIPT%
-if %ERRORLEVEL% EQU 0 ( :: generate-script.py 正常执行完成.
-    if not exist %BACKUP_SCRIPT% ( :: 合法项为 0, generate-script.py 不生成备份脚本.
-        echo 不存在合法的备份项! 请检查 conf\config.yml 文件的配置.
-        goto end
-    )
-
-    choice /c YN /m "已生成备份脚本. 确定执行备份吗 "
-    if ERRORLEVEL 2 (
-        echo 备份被取消.
-        goto end
-    )
-
-    echo 备份中...
-
-    if not exist %LOG_DIR% (
-        mkdir %LOG_DIR%
-    )else if exist %LOG% (
-        del %LOG%
-    )
-
-    %BACKUP_SCRIPT%
-    if %ERRORLEVEL% EQU -1 (
-        echo 备份被用户中断.
-    ) else (
-        echo 备份完成.
-    )
-
-    if exist %LOG% (
-        echo 简化日志...
-        python %SIMPLIFY_LOG_SCRIPT%
-    )
-) else (
-    echo 备份脚本生成失败! 请检查是否配置了有效的 Python 环境变量, 并安装了程序所需的模块.
+call:generateScript
+if %ERRORLEVEL% NEQ 0 (
+  goto end
 )
+
+choice /c YN /m "已生成备份脚本. 确定执行备份吗 "
+if ERRORLEVEL 2 (
+  echo 备份被取消.
+  goto end
+)
+
+call:initLog
+
+call:backupFolders
+if %ERRORLEVEL% NEQ 0 (
+  goto end
+)
+
+call:simplifyLog
+
+:generateScript
+  echo 根据 "config.yml" 生成备份脚本...
+  echo 将备份以下目录:
+  python %GENERATE_SCRIPT%
+
+  if %ERRORLEVEL% NEQ 0 (
+    echo 备份脚本生成失败! 请检查是否配置了有效的 Python 环境变量, 并安装了程序所需的模块.
+    exit /b 1
+  )
+
+  if not exist %BACKUP_SCRIPT% ( :: 合法项为 0, generate-script.py 不生成备份脚本.
+    echo 不存在合法的备份项! 请检查 conf\config.yml 文件的配置.
+    exit /b 1
+  )
+goto:eof
+
+:initLog
+  echo 初始化日志...
+  if not exist %LOG_DIR% (
+    mkdir %LOG_DIR%
+  )else if exist %LOG% (
+    del %LOG%
+  )
+goto:eof
+
+:backupFolders
+  echo 备份中...
+  call %BACKUP_SCRIPT%
+  if %ERRORLEVEL% EQU -1 (
+    echo 备份被用户中断.
+    exit /b -1
+  ) else if %ERRORLEVEL% NEQ 0 (
+    echo 备份失败!
+    exit /b 1
+  ) else (
+    echo 备份完成.
+  )
+goto:eof
+
+:simplifyLog
+  if exist %LOG% (
+    echo 简化日志...
+    python %SIMPLIFY_LOG_SCRIPT%
+  )
+goto:eof
 
 :end
 if exist %BACKUP_SCRIPT% (
-    del %BACKUP_SCRIPT%
+  del %BACKUP_SCRIPT%
 )
 pause
